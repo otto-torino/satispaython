5c98f652d66f943b20ae501f24bc03917922efdc
Daniele Pira <daniele.pira@otto.to.it>
2021-01-07 16:40:07 +0100

    add version support by project.toml

--- satispaython/__init__.py
+++ satispaython/__init__.py
@@ -1,3 +1,9 @@
 from ._api import obtain_key_id, test_authentication, create_payment, get_payment_details
 
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
 __all__ = [obtain_key_id, test_authentication, create_payment, get_payment_details]
