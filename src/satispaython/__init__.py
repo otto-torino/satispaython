9ad3f083910b3bca58a27ba1fd8abeba44b6a5fc
Daniele Pira <daniele.pira@otto.to.it>
2021-01-11 17:20:53 +0100

    lint __all__

--- src/satispaython/__init__.py
+++ src/satispaython/__init__.py
@@ -6,4 +6,4 @@ except ModuleNotFoundError:
     import importlib_metadata
 
 __version__ = importlib_metadata.version(__name__)
-__all__ = [obtain_key_id, test_authentication, create_payment, get_payment_details]
+__all__ = ["obtain_key_id", "test_authentication", "create_payment", "get_payment_details"]
